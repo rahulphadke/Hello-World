aa88127af930d012e2bb77e0e99d5d2acd930574
rahulphadke <gotorahul@gmail.com>
2015-07-06 15:22:29 +0530

    Welcome program in Java

---
+++ Welcome.java
@@ -0,0 +1,15 @@
+/*
+C:\Program Files (x86)\Java\jdk1.6.0_01\bin
+*/
+
+// Save the file with : Welcome.java
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello !!");
+		System.out.print(" Rahul");
+		System.out.println("**** Welcome to Wipro *****");
+
+		
+	}
+
+}
